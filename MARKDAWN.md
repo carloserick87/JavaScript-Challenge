c5bddd6e50d80421e412f73aca91fc342dbb79a4
Carlos Erick <ing.carloserick87@gmail.com>
2023-12-04 14:50:07 -0600

    First Challenge

---
+++ MARKDAWN.md
@@ -0,0 +1,7 @@
+# h1 Challenge: Variables and kind of Data
+
+1.Intenta escribir el código de JavaScript que ves en la consola. No tiene que ser perfecto🧑‍💻.
+
+2.Compara tu solucion con la solucion incluida en el archivo y descubre que partes pudiste de mejor manera. No te preocupes si encuentras métodos que no conocias, ya las conocerás 🙌 Buenas habilidades!! 🔥.
+
+[title](https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/site/2147489095/products/8f1ff2-82ce-6378-f522-706d6520623_Variables_y_tipos_de_datos.png)
---
+++ Reto_1.js
@@ -0,0 +1,17 @@
+//Variables statements
+let user = 'Carlos Erick';
+let message = 'Hello World!';
+let array = [1,2,3,4,5];
+array.push(6);
+let text1 = 'La lluvia en Sevilla es una maravilla'
+
+//
+
+console.log(`${message} I am ${user}`);
+console.log(`My name is Capitaletter ${user.toUpperCase()}`);
+console.log(`My name is Lowercase ${user.toLowerCase()}`);
+console.log(`This message ${message} has ${message.length} characters`);
+console.log(`The Third number array with the values'1,2,3,4,5' is: ${array[2]}`);
+console.log(`The number that follows in order is added to the end of the array and is ${array[5]}`);
+let busqueda = text1.match(/\s\w{6}\s/gi);
+console.log(`We found pattern \'${busqueda}\' in the text 'La lluvia en Sevilla es una maravilla', so the result is: ${busqueda}`);
